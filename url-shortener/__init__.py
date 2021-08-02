6cd7912a57f2f27ce07969d79a2e5eb49686836f
M Carlise <micarlise@gmail.com>
2021-08-02 19:24:06 -0400

    blueprint view for '/'
    cleans up app factory.  The views are now fully functional with mongodb
    backend.

--- url-shortener/__init__.py
+++ url-shortener/__init__.py
@@ -1,20 +1,12 @@
-from flask import Flask, request
+from flask import Flask
 from . import db
+from . import urls
 
 
 def create_app():
     app = Flask(__name__)
     db.client.init_db(app)
 
-    @app.route('/', methods=['GET'])
-    def get_all_codes():
-        return "<p>Get Codes</p>"
-
-    @app.route('/<string:short_url>', methods=['GET', 'POST'])
-    def get_code(short_url):
-        if request.method == 'GET':
-            return "<p>Get Code</p>"
-        else:
-            return "<p>Create Code </p>"
+    app.register_blueprint(urls.bp)
 
     return app
